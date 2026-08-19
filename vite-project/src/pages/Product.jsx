299cdd0be6f51ef35cf8f6616454aa5d1f9179d3
Himanshu6060 <himanshukumar34407@gmail.com>
2026-08-20 00:32:24 +0530

    Update Product.jsx

--- vite-project/src/pages/Product.jsx
+++ vite-project/src/pages/Product.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import products from "src/data/products.json";
+import products from "../data/products.json";
 
 
 function Product() {
